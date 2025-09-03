f649c381243196377600723299a2451e40e03539
Roy A Vanegas <roy@thecodeeducators.com>
2025-09-02 21:40:10 -0400

    Add program folders

---
+++ program-1/Assignment1Program1.java
@@ -0,0 +1,5 @@
+public class Assignment1Program1 {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ program-2/Assignment1Program2.java
@@ -0,0 +1,5 @@
+public class Assignment1Program2 {
+  public static void main(String[] args) {
+
+  }
+}
